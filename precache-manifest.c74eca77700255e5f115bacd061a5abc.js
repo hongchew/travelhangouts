601127f5dcde2b7af5ca9480f09d9360a6fe1946
Toh Hong Chew <hongchew@u.nus.edu>
2020-06-29 22:02:13 +0800

    Updates

---
+++ precache-manifest.c74eca77700255e5f115bacd061a5abc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96059f72e9c47ac6259e605569737f56",
+    "url": "/travelhangouts/index.html"
+  },
+  {
+    "revision": "1a6952fcda0d772764db",
+    "url": "/travelhangouts/static/css/main.8a8444d9.chunk.css"
+  },
+  {
+    "revision": "f3bf87157d9237e000fa",
+    "url": "/travelhangouts/static/js/2.008b3d6d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/travelhangouts/static/js/2.008b3d6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a6952fcda0d772764db",
+    "url": "/travelhangouts/static/js/main.9a50574b.chunk.js"
+  },
+  {
+    "revision": "ef18faaaa1e75b9bc0fe",
+    "url": "/travelhangouts/static/js/runtime-main.4eaab68f.js"
+  },
+  {
+    "revision": "5a720a5eeb453817eb73b8b6deb41d05",
+    "url": "/travelhangouts/static/media/Solo-2.5a720a5e.jpg"
+  },
+  {
+    "revision": "23debf1a843de1f1d98901a8a2e63cb0",
+    "url": "/travelhangouts/static/media/logo-transparent.23debf1a.png"
+  },
+  {
+    "revision": "f7c21ae3b4e684842356b7a43fc38612",
+    "url": "/travelhangouts/static/media/safety.f7c21ae3.png"
+  },
+  {
+    "revision": "a152b2543241a6753f002f0a8cede0ad",
+    "url": "/travelhangouts/static/media/user-flow.a152b254.png"
+  },
+  {
+    "revision": "c64d19b5deb41100dafa52cc1a467b69",
+    "url": "/travelhangouts/static/media/value.c64d19b5.png"
+  }
+]);
